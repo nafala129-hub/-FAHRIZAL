4752d5d07ad8ede345eb0c4f167cf61857c91a6b
nafala129-hub <nafala129@gmail.com>
2025-10-23 10:17:51 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+// 1. pilih tombol berdasarkan ID-nya
+const tombol sapa = document.getElementById(`sapa button`)
+// 2. tambahkan `event listener` untuk aksi `click`
+tombolsapa.addEventListener(`click`, function() {
+    // 3. jalankan kode ini ketika tombol ini di-klik
+    alert(` Halo! terima kasih sudah ber kunjung!`);
+});
